c5193a3abb5b3a84484d97a5160168bfa9c35379
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2024-01-13 20:27:10 -0300

    update page products
    adicionando pagina para os produtos

---
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
+
+export const Breadcrum = (props) => {
+    const {product} = props;
+  return (
+    <div className='breadcrum'>
+        HOME <img src={arrow_icon} alt="arrowIcon" /> SHOP <img src={arrow_icon} alt="arrowIcon" /> 
+        {product.category} <img src={arrow_icon} alt="arrowIcon" /> 
+        {product.name}
+    </div>
+  )
+}
